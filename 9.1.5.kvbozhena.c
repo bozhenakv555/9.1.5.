a8c7edde30652ded2b2777ebfaad01efae885569
bozhenakv <bkvartuk@gmail.com>
2025-05-31 20:10:58 +0300

    Pridana funkcia na vytvorenie prazdnej matice s dynamickou alokaciou pamate, ktora dostane na vstup pocet riadkov a stlpcov a alokuje miesto pre prvky matice a vrati NULL, ak sa nepodari alokovat pamat.

--- 9.1.5.kvbozhena.c
+++ 9.1.5.kvbozhena.c
@@ -12,3 +12,19 @@ float *elem;
 MAT mat;
 
 #define ELEM(mat,i,j) ((mat).elem[(i)*(mat).cols+(j)])
+
+MAT *mat_create_with_type(unsigned int rows, unsigned int cols){
+	MAT *mat = (MAT*)malloc(sizeof(MAT));
+	if (mat == NULL){
+		return NULL;
+	}
+	mat->rows = rows;
+	mat->cols = cols;
+	mat->elem = (float*)malloc(rows*cols*sizeof(float));
+	if(mat->elem == NULL){
+		free(mat);
+		return NULL;
+	}
+	return mat;
+}
+
