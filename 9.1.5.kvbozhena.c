cd69e3edc65aadb656190a8ad0a1f0cbd8ba3c49
bozhenakv <bkvartuk@gmail.com>
2025-06-01 04:50:08 +0300

    Pridana funkcia na naplnenie alokovanej matice nahodnymi celymi cislami z intervalu [min, max]. Funkcia este bude podliehat upravam.

--- 9.1.5.kvbozhena.c
+++ 9.1.5.kvbozhena.c
@@ -45,6 +45,16 @@ int mat_is_square_or_not(MAT *mat){
 	}
 }
 
+void mat_generate_random_int_elems (MAT *mat, int min, int max){
+	for (unsigned int i = 0; i < mat->rows; i++){
+		for(unsigned int j = 0; j < mat->cols; j++){
+			ELEM(*mat,i,j) = min + rand()% (max-min+1);
+		}
+	}
+}
+
 char mat_create_random_unimodular_integer(MAT *mat){
-	if(mat_is_square_or_not(mat)==1)
+	if(mat == NULL && mat_is_square_or_not(mat) == 0){
+		return 0;
+	}
 }
